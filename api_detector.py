63d8ca5ae7c2704555a1d6ee1760c9c7ac33d45d
Elmo Ranolo <elmoranolo@gmail.com>
2023-12-03 20:43:36 +0800

    able to display and loaod json file

--- api_detector.py
+++ api_detector.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
@@ -6,18 +7,25 @@ app = Flask(__name__)
 
 #After process
 # Sample jason data
-class_type = {
-    "contents": [
-        "Seagrass",
-        "Coral",
-        "Seaweeds"
-    ]
-}
+# class_type = {
+#     "contents": [
+#         "Seagrass",
+#         "Coral",
+#         "Seaweeds"
+#     ]
+# }
 
-# Route to get all data
+
+# Load JSON file
+json_file_path = r'C:\Users\Admin\Documents\TrainYourOwnYOLO\Data\Source_Images\Test_Image_Detection_Results/grouped_detection_results_by_image.json'
+with open(json_file_path, 'r') as json_file:
+    data = json.load(json_file)
+
+
+# Route to get all data old setup
 @app.route('/api/contents', methods=['GET'])    
 def get_all_data():
-    return jsonify({"contents": class_type})
+    return jsonify({"contents": data})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5002)
